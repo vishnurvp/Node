7781352c5b2ed07b5895ce5374db0dceb6f643c2
vishnurvp <vishnuprasad.rvp@gmail.com>
2022-10-06 16:41:22 +0530

    kikstarting task 7

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((request, response) => {
-  console.log(request);
+  const url = request.url;
+  const method = request.method;
+  response.setHeader('Content-Type', 'text/html');
+  if (url === '/') {
+    response.write('<html>');
+    response.write('<head><title>Home Page</title></head>');
+    response.write(
+    `<body>
+    Hello from node js server
+    <h1>Enter your name</h1>
+    <br/>
+    <form action='/message' method='POST'>
+    <input type='text' name='name'/>
+    <button type='submit'>Submit</button>
+    </form>
+    </body>`);
+    response.write('</html>');
+    return response.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'Dummy');
+    response.statusCode = 302;
+    response.setHeader('Location', '/');
+    return response.end();
+  }
+
+  if (url === '/home') {
+    response.write('<h1>Welcome Home</h1>');
+    return response.end();
+  }
+
+  if (url === '/about') {
+    response.write('<h1>Welcome to about yourself</h1>');
+    return response.end();
+  }
+
+  if (url === '/node') {
+    response.write('<h1>Welcome to Node js server project</h1>');
+    return response.end();
+  }
+  
 });
 
 server.listen(4000, ()=>{
